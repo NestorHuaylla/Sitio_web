3436e7af91b13164c5ef48a1f23fc6b0478d8c31
NestorHuaylla <nestorfernandohuayllabenito0@gmail.com>
2025-10-17 16:24:07 -0500

    quinto comit

---
+++ PracticasBootStrap/script.js
@@ -0,0 +1,4 @@
+// Ejemplo de acción al hacer clic en "Guardar cambios"
+document.getElementById("guardarCambios").addEventListener("click", function() {
+  alert("¡Cambios guardados exitosamente!");
+});
